7071c40591fe5cb7cb6522f3406ef11066eeae3e
Roopesh Vishwakarma <123719841+Roopesh127@users.noreply.github.com>
2023-07-03 20:43:02 +0530

    Add files via upload

---
+++ object.js
@@ -0,0 +1,68 @@
+// const profile = {
+//   name: "Sushmitha",
+//   city: "Chennai",
+//   state: "Tamil Nadu",
+//   country: "India",
+//   pincode: 6787445,
+//   isActive: true,
+//   hobbies: ["Yoga", "Baharatnatyam"],
+//   details: {
+//     graduation: "Yes",
+//     postGraduation: null,
+//   },
+// };
+
+// // Reading the values
+// console.log(profile.name); // Sushmitha
+// console.log(profile.city); // Chennai
+// console.log(profile["city"]); // Chennai
+// console.log(profile.contactNo); // undefined
+// console.log(profile.details.graduation); // Yes
+// console.log(profile["details"]["graduation"]); // Yes
+// console.log(profile.details.postGraduation); // null
+
+
+
+
+// const icard = {
+//     name: "Roopesh",
+//     cast:"Vishwakarma",
+//     city:"Jabalpur",
+//     MN:9302017774,
+//     education: null,
+// }
+
+// console.log(icard.education);
+// console.log(icard.MN);
+
+
+
+// const profile = {
+//     name: "One",
+//     city: "ABC",
+//   };
+  
+//   // Add new data
+//   profile.country = "USA";
+//   profile["country"] = "India";
+  
+//   // Update existing data
+//   profile.city = "Goa";
+  
+//   console.log("profile", profile);
+//   console.log("profile.country", profile.country);
+  
+
+
+const Apka_bank = {
+    Acc: "8u85858980",
+    Acc: "erhgiueryg9tut9t3tjn",
+    Acc: "wfeh74r34809jg o rugorj"
+
+}
+Apka_bank.balance = 24982389;
+Apka_bank.Branch = "FAtehpur";
+Apka_bank.Acc = "qndkjqhdqhdn";
+
+console.log(Apka_bank.Acc)
+console.log(Apka_bank.Branch.Acc);
